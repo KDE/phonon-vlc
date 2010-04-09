d4313ac73dafd8d43e62a6b90269db036d9ef3ab
Mark Kretschmann <kretschmann@kde.org>
2010-04-09 09:02:19 +0200

    Fix encoding of some URLs.

--- vlc/vlcmediaobject.cpp
+++ vlc/vlcmediaobject.cpp
@@ -87,7 +87,7 @@ void VLCMediaObject::loadMediaInternal(const QString & filename)
 {
     qDebug() << __FUNCTION__ << filename;
 
-    p_current_file = QUrl::toPercentEncoding(filename, ":/");
+    p_current_file = QUrl::toPercentEncoding(filename, ":/?=");
 
     // Why is this needed???
     emit stateChanged(Phonon::StoppedState);
