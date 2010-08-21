e9c8170eaf87e985c219cc715bfcff8b85516505
Harald Sitter <sitter@kde.org>
2010-08-21 07:26:42 +0200

    include own header first

--- vlc/streamhooks.cpp
+++ vlc/streamhooks.cpp
@@ -20,8 +20,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA *
  *****************************************************************************/
 
-#include "streamreader.h"
 #include "streamhooks.h"
+#include "streamreader.h"
 
 #define BLOCKSIZE 32768
 
