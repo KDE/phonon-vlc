62fff009a4485957b5c336249f4be4842075aa19
Harald Sitter <sitter@kde.org>
2010-10-12 17:16:35 +0200

    Try to emit aboutToFinish() before finish(), to make sure it was emitted at all. Amarok currently depends on aboutToFinish to be emitted to proceed in the playlist.

--- vlc/vlcmediaobject.cpp
+++ vlc/vlcmediaobject.cpp
@@ -439,6 +439,7 @@ void VLCMediaObject::libvlc_callback(const libvlc_event_t *p_event, void *p_user
     if (p_event->type == libvlc_MediaPlayerEndReached && !p_vlc_mediaObject->checkGaplessWaiting()) {
         i_first_time_media_player_time_changed = 0;
         p_vlc_mediaObject->clearMediaController();
+        p_vlc_mediaObject->emitAboutToFinish();
         emit p_vlc_mediaObject->finished();
         emit p_vlc_mediaObject->stateChanged(Phonon::StoppedState);
     } else if (p_event->type == libvlc_MediaPlayerEndReached) {
