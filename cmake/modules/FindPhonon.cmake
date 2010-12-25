637a4348654787406be0883e9f41e48b627c58de
Harald Sitter <sitter@kde.org>
2010-12-25 21:06:23 +0100

    Poor phonon-vlc is now my testing ground for the new build system ;)
    Delete all files that are now distributed by phonon as part of the buildsystem. Also cleanup cmake files accordingly. Please come see me if that makes things break (also note that you will need Phonon git from a couple of minutes ago).

--- cmake/modules/FindPhonon.cmake
+++ cmake/modules/FindPhonon.cmake
@@ -1,110 +1,53 @@
-# Find libphonon
-# Once done this will define
-#
-#  PHONON_FOUND                 - system has Phonon Library
-#  PHONON_FOUND_EXPERIMENTAL    - system also has Phonon Experimental Library
-#  PHONON_INCLUDES              - the Phonon include directory
-#  PHONON_LIBS                  - link these to use Phonon
-#  PHONON_VERSION               - the version of the Phonon Library
+# Find Phonon
 
-# Copyright (c) 2008, Matthias Kretz <kretz@kde.org>
-# Copyright (c) 2010, Mark Kretschmann <kretschmann@kde.org>
+# Copyright (c) 2010, Harald Sitter <sitter@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-macro(_phonon_find_version)
-   if (APPLE AND EXISTS "${PHONON_INCLUDE_DIR}/Headers/phononnamespace.h")
-      set(_phonon_namespace_header_file "${PHONON_INCLUDE_DIR}/Headers/phononnamespace.h")
-      set(_phonon_pulsesupport_header_file "${PHONON_INCLUDE_DIR}/Headers/pulsesupport.h")
-   else ()
-      set(_phonon_namespace_header_file "${PHONON_INCLUDE_DIR}/phonon/phononnamespace.h")
-      set(_phonon_pulsesupport_header_file "${PHONON_INCLUDE_DIR}/phonon/pulsesupport.h")
-   endif (APPLE AND EXISTS "${PHONON_INCLUDE_DIR}/Headers/phononnamespace.h")
+if (NOT PHONON_BUILDSYSTEM_DIR)
+    find_program(PC_EXECUTABLE NAMES pkg-config
+    PATH_SUFFIXES bin               
+    HINTS
+    ${CMAKE_INSTALL_PREFIX}
+    ONLY_CMAKE_FIND_ROOT_PATH
+    )
 
-   file(READ ${_phonon_namespace_header_file} _phonon_header LIMIT 5000 OFFSET 1000)
-   string(REGEX MATCH "define PHONON_VERSION_STR \"(4\\.[0-9]+\\.[0-9a-z]+)\"" _phonon_version_match "${_phonon_header}")
-   set(PHONON_VERSION "${CMAKE_MATCH_1}")
-   message(STATUS "Phonon Version: ${PHONON_VERSION}")
-endmacro(_phonon_find_version)
+    if (NOT PC_EXECUTABLE)
+        if (Phonon_FIND_REQUIRED)
+            message(FATAL_ERROR "ERROR: Could not find pkg-config [required to find Phonon].")
+        endif (Phonon_FIND_REQUIRED)
+    endif (NOT PC_EXECUTABLE)
 
-if(PHONON_FOUND)
-   # Already found, nothing more to do except figuring out the version
-   _phonon_find_version()
-else(PHONON_FOUND)
-   if(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
-      set(PHONON_FIND_QUIETLY TRUE)
-   endif(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
+    if (PC_EXECUTABLE)
+        execute_process(COMMAND "${PC_EXECUTABLE}" --variable=buildsystemdir phonon
+                        OUTPUT_VARIABLE _data_DIR
+                        ERROR_QUIET
+                        OUTPUT_STRIP_TRAILING_WHITESPACE)
+        file(TO_CMAKE_PATH "${_data_DIR}" _data_DIR)
+        find_path(PHONON_BUILDSYSTEM_DIR FindPhononInternal.cmake HINTS ${_data_DIR})
+    endif (PC_EXECUTABLE)
+endif (NOT PHONON_BUILDSYSTEM_DIR)
 
-   # As discussed on kde-buildsystem: first look at CMAKE_PREFIX_PATH, then at the suggested PATHS (kde4 install dir)
-   find_library(PHONON_LIBRARY NAMES phonon PATHS ${KDE4_LIB_INSTALL_DIR} ${QT_LIBRARY_DIR} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-   # then at the default system locations (CMAKE_SYSTEM_PREFIX_PATH, i.e. /usr etc.)
-   find_library(PHONON_LIBRARY NAMES phonon)
+if (PHONON_BUILDSYSTEM_DIR)
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PHONON_BUILDSYSTEM_DIR})
 
-   find_library(PHONON_LIBRARY_EXPERIMENTAL NAMES phononexperimental PATHS ${KDE4_LIB_INSTALL_DIR} ${QT_LIBRARY_DIR} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-   find_library(PHONON_LIBRARY_EXPERIMENTAL phononexperimental)
+    if (Phonon_FIND_REQUIRED)
+        set(_req REQUIRED)
+    endif (Phonon_FIND_REQUIRED)
+    if (PHONON_FIND_QUIETLY)
+        set(_quiet QUIET)
+    endif (PHONON_FIND_QUIETLY)
 
-   find_path(PHONON_INCLUDE_DIR NAMES phonon/phonon_export.h PATHS ${KDE4_INCLUDE_INSTALL_DIR} ${QT_INCLUDE_DIR} ${INCLUDE_INSTALL_DIR} ${QT_LIBRARY_DIR} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-   find_path(PHONON_INCLUDE_DIR NAMES phonon/phonon_export.h)
-
-   find_path(PHONON_INCLUDE_DIR_EXPERIMENTAL NAMES phonon/experimental/export.h PATHS ${KDE4_INCLUDE_INSTALL_DIR} ${QT_INCLUDE_DIR} ${INCLUDE_INSTALL_DIR} ${QT_LIBRARY_DIR} NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH)
-   find_path(PHONON_INCLUDE_DIR_EXPERIMENTAL NAMES phonon/experimental/export.h)
-
-   if(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
-      set(PHONON_LIBS ${phonon_LIB_DEPENDS} ${PHONON_LIBRARY})
-      set(PHONON_INCLUDES ${PHONON_INCLUDE_DIR}/KDE ${PHONON_INCLUDE_DIR})
-      set(PHONON_FOUND TRUE)
-      _phonon_find_version()
-      find_path(PHONON_PULSESUPPORT NAMES phonon/pulsesupport.h PATHS ${PHONON_INCLUDES})
-
-      if(PHONON_PULSESUPPORT)
-         # Check if the method enable() is available in pulsesupport.h
-         file(READ ${_phonon_pulsesupport_header_file} _pulsesupport_header)
-         string(REGEX MATCH "void enable" _phonon_pulse_match "${_pulsesupport_header}")
-         set(PHONON_PULSESUPPORT "${_phonon_pulse_match}")
-      endif(PHONON_PULSESUPPORT)
-
-      if(PHONON_INCLUDE_DIR_EXPERIMENTAL AND PHONON_LIBRARY_EXPERIMENTAL)
-         set(PHONON_LIBS ${PHONON_LIBS} ${PHONON_LIBRARY_EXPERIMENTAL})
-         set(PHONON_INCLUDES ${PHONON_INCLUDES} ${PHONON_INCLUDE_DIR_EXPERIMENTAL})
-         set(PHONON_FOUND_EXPERIMENTAL TRUE)
-         message(STATUS "Found Phonon Experimental: ${PHONON_LIBRARY_EXPERIMENTAL}")
-      endif(PHONON_INCLUDE_DIR_EXPERIMENTAL AND PHONON_LIBRARY_EXPERIMENTAL)
-
-   else(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
-      set(PHONON_FOUND FALSE)
-   endif(PHONON_INCLUDE_DIR AND PHONON_LIBRARY)
-
-   if(PHONON_FOUND)
-      if(NOT PHONON_FIND_QUIETLY)
-         message(STATUS "Found Phonon: ${PHONON_LIBRARY}")
-         message(STATUS "Found Phonon Includes: ${PHONON_INCLUDES}")
-         if(PHONON_PULSESUPPORT)
-            message(STATUS "Found Phonon PulseAudio Support: Yes")
-         else(PHONON_PULSESUPPORT)
-            message(STATUS "Found Phonon PulseAudio Support: No")
-         endif(PHONON_PULSESUPPORT)
-      endif(NOT PHONON_FIND_QUIETLY)
-   else(PHONON_FOUND)
-      if(Phonon_FIND_REQUIRED)
-         if(NOT PHONON_INCLUDE_DIR)
-            message(STATUS "Phonon includes NOT found!")
-         endif(NOT PHONON_INCLUDE_DIR)
-         if(NOT PHONON_LIBRARY)
-            message(STATUS "Phonon library NOT found!")
-         endif(NOT PHONON_LIBRARY)
-         message(FATAL_ERROR "Phonon library or includes NOT found!")
-      else(Phonon_FIND_REQUIRED)
-         message(STATUS "Unable to find Phonon")
-      endif(Phonon_FIND_REQUIRED)
-   endif(PHONON_FOUND)
-
-
-   mark_as_advanced(
-        PHONON_INCLUDE_DIR
-        PHONON_INCLUDE_DIR_EXPERIMENTAL
-        PHONON_LIBRARY
-        PHONON_LIBRARY_EXPERIMENTAL
-        PHONON_INCLUDES
-   )
-endif(PHONON_FOUND)
+    find_package(PhononInternal ${_req} ${_quiet})
+else (PHONON_BUILDSYSTEM_DIR)
+    if (_data_DIR)
+        if (Phonon_FIND_REQUIRED)
+            message(FATAL_ERROR "ERROR: FindPhonon.cmake not found in ${_data_DIR}")
+        endif (Phonon_FIND_REQUIRED)
+    else (_data_DIR)
+        if (Phonon_FIND_REQUIRED)
+            message(FATAL_ERROR "ERROR: Either pkg-config can not find its phonon config, or you are not using a recent enough Phonon version.")
+        endif (Phonon_FIND_REQUIRED)
+    endif (_data_DIR)
+endif (PHONON_BUILDSYSTEM_DIR)
