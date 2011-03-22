c9662035c3cd7fd5a258c2e729a0d128151da6df
Albert Astals Cid <aacid@kde.org>
2011-03-22 19:13:53 +0000

    Fix Messages.sh
    Now it "works aka extracts the message", but i don't see you loading the phonon-vlc catalog so probably won't be translated anyway
    CCMAIL: sitter@kde.org

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-find gstreamer/ -maxdepth 1 -name "*.cpp" -print > files
-find gstreamer/ -maxdepth 1 -name "*.h" -print >> files
+find vlc/ -maxdepth 1 -name "*.cpp" -print > files
+find vlc/ -maxdepth 1 -name "*.h" -print >> files
 $XGETTEXT_QT --copyright-holder=This_file_is_part_of_KDE --msgid-bugs-address=http://bugs.kde.org --files-from=files -o $podir/phonon_vlc.pot
 rm files
