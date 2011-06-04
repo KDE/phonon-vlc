c12334c28397e1b6bc20ac95a9362eb4652f6757
Pino Toscano <pino@kde.org>
2011-06-04 10:50:10 +0200

    Messages.sh: simplify and make it work again

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,4 @@
 #! /usr/bin/env bash
-find vlc/ -maxdepth 1 -name "*.cpp" -print > files
-find vlc/ -maxdepth 1 -name "*.h" -print >> files
+find src/ -maxdepth 1 -name "*.cpp" -print > files
 $XGETTEXT_QT --copyright-holder=This_file_is_part_of_KDE --msgid-bugs-address=http://bugs.kde.org --files-from=files -o $podir/phonon_vlc.pot
-rm files
+rm -f files
