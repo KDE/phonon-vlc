6f06e8f96532f13677741e0b41e4f588de5fe006
Harald Sitter <sitter@kde.org>
2011-07-27 12:45:00 +0200

    do not segfault when accessing an unitialized libvlc... this prevents crashes in the backend ctor, thus allowing for apps to be used even if vlc is kaput

--- src/devicemanager.cpp
+++ src/devicemanager.cpp
@@ -170,7 +170,7 @@ void DeviceManager::updateDeviceList()
     audioOutputDeviceList.append(DeviceInfo("default"));
     audioOutputDeviceList.last().capabilities = DeviceInfo::AudioOutput;
 
-    if (!LibVLC::self)
+    if (!LibVLC::self || !libvlc)
         return;
 
     // Get the list of available audio outputs
