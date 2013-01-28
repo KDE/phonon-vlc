092ab78664587acee1a3fea53a75dfd418b1bf81
Harald Sitter <sitter@kde.org>
2013-01-28 04:43:25 +0100

    component is called Phonon4Qt5 not Phonon4Support

--- cmake/modules/FindPhonon.cmake
+++ cmake/modules/FindPhonon.cmake
@@ -19,4 +19,4 @@ endif()
 
 include(FindPackageHandleStandardArgs)
 
-find_package_handle_standard_args(Phonon4Support  DEFAULT_MSG  Phonon4Qt5_DIR )
+find_package_handle_standard_args(Phonon4Qt5  DEFAULT_MSG  Phonon4Qt5_DIR )
