3e86f5c95e570013ede29e22ff5f46a258e3b85f
René J.V. Bertin <rjvbertin@gmail.com>
2017-02-20 10:37:07 +0100

    backport of 2 commits for Mac on master:
        0d32bc8358436429fff9251377d86bb03c2aaf1f
            suppress a senseless simulated debug/warning message
            The Mac video widget delegate printed an incomprehensible and
            irrelevant vout log message on stdout. Removed.
        5768c95dd0ee6f12d38efcbe8dcf5d6f3f74a9f1
            work around an over-releasing regression in Qt 5.8.0
            Also: use @autoreleasepool instead of an explicit
            NSAutoreleasePool instance.

--- src/video/mac/vlcmacwidget.mm
+++ src/video/mac/vlcmacwidget.mm
@@ -25,18 +25,15 @@ VlcMacWidget::VlcMacWidget(QWidget *parent) : QMacCocoaViewContainer(0, parent)
 {
     // Many Cocoa objects create temporary autorelease objects,
     // so create a pool to catch them.
+    @autoreleasepool {
+        VideoView *videoView = [[VideoView alloc] init];
 
-    printf("[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.[0x123c1b938] vout_macosx generic debug: No drawable-nsobject, passing over.");
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    VideoView *videoView = [[VideoView alloc] init];
+        this->setCocoaView(videoView);
 
-    this->setCocoaView(videoView);
-
-    // Release our reference, since our super class takes ownership and we
-    // don't need it anymore.
-    [videoView release];
-
-    // Clean up our pool as we no longer need it.
-    [pool release];
-
+        // Release our reference, since our super class takes ownership and we
+        // don't need it anymore (except with Qt 5.8.0 because of a regression).
+        if (strcmp(qVersion(), "5.8.0")) {
+            [videoView release];
+        }
+    }
 }
