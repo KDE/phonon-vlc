9a4ce3b0613d41361529fdbfde89d68a991f6f5b
l10n daemon script <scripty@kde.org>
2017-11-22 04:01:45 +0100

    SVN_SILENT made messages (.desktop file)

--- src/vlc.desktop.cmake
+++ src/vlc.desktop.cmake
@@ -12,11 +12,17 @@ InitialPreference=20
 Name=VLC
 Name[ca]=VLC
 Name[ca@valencia]=VLC
+Name[es]=VLC
+Name[fr]=VLC
 Name[nl]=VLC
+Name[sk]=VLC
 Name[x-test]=xxVLCxx
 
 Comment=Phonon VLC backend
 Comment[ca]=Dorsal del VLC pel Phonon
 Comment[ca@valencia]=Dorsal del VLC pel Phonon
+Comment[es]=Motor VLC para Phonon
+Comment[fr]=Module VLC pour Phonon
 Comment[nl]=VLC-backend van Phonon
+Comment[sk]=Backend Phonon VLC
 Comment[x-test]=xxPhonon VLC backendxx
