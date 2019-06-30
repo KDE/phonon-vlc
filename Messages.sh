ff3e27ec93c40bb86ae523e61213921b1038a2d7
Pino Toscano <pino@kde.org>
2019-06-30 12:09:08 +0200

    i18n: use https for bugs.kde.org

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 find src/ -maxdepth 1 -name "*.cpp" -print > files
-$XGETTEXT_QT --copyright-holder=This_file_is_part_of_KDE --msgid-bugs-address=http://bugs.kde.org --files-from=files -o $podir/phonon_vlc.pot
+$XGETTEXT_QT --copyright-holder=This_file_is_part_of_KDE --msgid-bugs-address=https://bugs.kde.org --files-from=files -o $podir/phonon_vlc.pot
 rm -f files
